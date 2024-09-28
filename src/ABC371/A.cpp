69abe85eecada70720e7fa14d58b625719c8f71c
lambda <shumpeta.kus@gmail.com>
2024-09-28 21:48:07 +0900

    ABC371 A

---
+++ src/ABC371/A.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string sab;
+  string sac;
+  string sbc;
+
+  cin >> sab;
+  cin >> sac;
+  cin >> sbc;
+
+  if (sab == "<" && sac == ">")
+  {
+    cout << "A" << endl;
+  }
+
+  if (sab == "<" && sbc == "<")
+  {
+    cout << "B" << endl;
+  }
+
+  if (sab == ">" && sac == "<")
+  {
+    cout << "A" << endl;
+  }
+
+  if (sab == ">" && sbc == ">")
+  {
+    cout << "B" << endl;
+  }
+
+  if (sac == "<" && sbc == ">")
+  {
+    cout << "C" << endl;
+  }
+
+  if (sac == ">" && sbc == "<")
+  {
+    cout << "C" << endl;
+  }
+  return 0;
+}
