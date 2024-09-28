51a9e6b3f7bb9233d692c90df6d9405602f6d7af
lambda <shumpeta.kus@gmail.com>
2024-09-28 22:46:27 +0900

    ABC371 B

---
+++ src/ABC371/B.cpp
@@ -0,0 +1,43 @@
+#include <atcoder/all>
+#include <iostream>
+
+using namespace std;
+using namespace atcoder;
+
+using mint = modint998244353;
+
+int main()
+{
+  int N;
+  int M;
+
+  cin >> N >> M;
+
+  vector<int> A(M);
+  vector<string> B(M);
+
+  for (int i = 0; i < M; i++)
+  {
+    cin >> A[i];
+    cin >> B[i];
+  }
+
+  vector<bool> C(N);
+  vector<string> D(M, "No");
+
+  for (int i = 0; i < M; i++)
+  {
+    if (B[i] == "M" && !C[A[i]])
+    {
+      C[A[i]] = true;
+      D[i] = "Yes";
+    }
+  }
+
+  for (int i = 0; i < M; i++)
+  {
+    cout << D[i] << endl;
+  }
+
+  return 0;
+}
