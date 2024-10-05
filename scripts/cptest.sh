a28a070caf1787970de35990f4f73a020b62b0db
lambda <shumpeta.kus@gmail.com>
2024-10-06 02:32:09 +0900

    ojコマンド初期化スクリプト追加

--- scripts/cptest.sh
+++ scripts/cptest.sh
@@ -4,6 +4,14 @@ workspace=$1
 dirname=$2
 filename=$3
 
+# コマンドを確認して存在しない場合インストール
+if !(type "oj" > /dev/null 2>&1); then
+  pip install --upgrade pip
+  pip install online-judge-tools
+  pip install setuptools
+  pip install selenium
+fi
+
 # 作成コードをコンパイル & 実行スクリプト
 compile_script="g++ $dirname/$filename.cpp -o $dirname/out/$filename -std=c++17 -I $workspace/include"
 exe_script="$dirname/out/$filename"
