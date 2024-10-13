364c230d5688a0684c339e08bfc757ac60b87c67
lambda <shumpeta.kus@gmail.com>
2024-10-13 18:00:44 +0900

    K solve

--- src/practice2/K.cpp
+++ src/practice2/K.cpp
@@ -6,36 +6,49 @@ using namespace std;
 
 using mint = modint998244353;
 
+struct HierarchicalMint {
+  mint val;
+  int hierarchy;
+};
+
 struct Functional {
   mint b;
   mint c;
 
   Functional(mint b, mint c) : b(b), c(c) {}
-  mint apply(mint x) { return b * x + c; }
+  HierarchicalMint apply(HierarchicalMint x) {
+    mint hierarchy = x.hierarchy;
+    return HierarchicalMint{b * x.val + hierarchy * c, x.hierarchy};
+  }
 };
 
-mint op(mint a, mint b) { return a + b; }
-
-mint e() { return 0; }
-
-mint mapping(Functional f, mint x) { return f.apply(x); }
-
-Functional composition(Functional f, Functional g) {
-  return Functional(f.b * g.b, f.b * g.c + f.c);
+HierarchicalMint op(HierarchicalMint a, HierarchicalMint b) {
+  return HierarchicalMint{a.val + b.val, a.hierarchy + b.hierarchy};
 }
 
-Functional id() { return Functional(1, 0); }
+HierarchicalMint e() { return HierarchicalMint{0, 1}; }
+
+HierarchicalMint mapping(Functional f, HierarchicalMint x) {
+  return f.apply(x);
+}
+
+Functional composition(Functional f, Functional g) {
+  return Functional{f.b * g.b, f.b * g.c + f.c};
+}
+
+Functional id() { return Functional{1, 0}; }
 
 int main() {
   int n, q;
   cin >> n >> q;
 
-  lazy_segtree<mint, op, e, Functional, mapping, composition, id> tree(n);
+  lazy_segtree<HierarchicalMint, op, e, Functional, mapping, composition, id>
+      tree(n);
 
   for (int i = 0; i < n; i++) {
     int a;
     cin >> a;
-    tree.set(i, a);
+    tree.set(i, HierarchicalMint{a, 1});
   }
 
   for (int i = 0; i < q; i++) {
@@ -48,8 +61,7 @@ int main() {
     }
 
     if (t == 1) {
-      mint ans = tree.prod(l, r);
-      cout << ans.val() << endl;
+      cout << tree.prod(l, r).val.val() << endl;
     }
   }
 
