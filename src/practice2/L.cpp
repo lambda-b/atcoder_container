a21a8fcbaea62591db58fd1e15814e82969fe0d2
lambda <shumpeta.kus@gmail.com>
2024-10-13 22:56:15 +0900

    L solve

--- src/practice2/L.cpp
+++ src/practice2/L.cpp
@@ -7,21 +7,27 @@ using namespace atcoder;
 using namespace std;
 
 struct model {
-  int a;
-  int w;
-  int v;
+  /** 区間内の1の個数 */
+  long a;
 
-  model add(model &other) {
-    int a0 = this->a + other.a;
-    int w0 = this->w + other.w;
-    int v0 = this->v + other.v + (other.w - other.a) * this->a;
+  /** 区間の幅 - (w - a)で0の個数になる */
+  long w;
+
+  /** 区間の転倒数 */
+  long v;
+
+  model add(model &other) const {
+    long a0 = this->a + other.a;
+    long w0 = this->w + other.w;
+    long v0 = this->v + other.v + (other.w - other.a) * this->a;
     return model{a0, w0, v0};
   }
 
-  model reverse() {
-    int a0 = this->w - this->a;
-    int w0 = this->w;
-    int v0 = this->a * (this->w - this->a) - this->v;
+  model reverse() const {
+    long a0 = this->w - this->a;
+    long w0 = this->w;
+    long v0 = this->a * (this->w - this->a) - this->v;
+
     return model{a0, w0, v0};
   }
 
@@ -31,11 +37,12 @@ struct model {
 };
 
 struct functional {
+  /** 反転するかどうか */
   bool b;
 
   functional(bool b) : b(b){};
 
-  model apply(model &m) { return b ? m.reverse() : m; }
+  model apply(model &m) const { return b ? m.reverse() : m; }
 
   static model mapping(functional f, model m) { return f.apply(m); }
 
@@ -47,7 +54,7 @@ struct functional {
 };
 
 int main() {
-  int n, q;
+  long n, q;
   cin >> n >> q;
 
   lazy_segtree<model, model::op, model::e,  //
@@ -55,14 +62,14 @@ int main() {
                functional::id>
       tree(n);
 
-  for (int i = 0; i < n; i++) {
-    int a;
+  for (long i = 0; i < n; i++) {
+    long a;
     cin >> a;
     tree.set(i, model{a, 1, 0});
   }
 
-  for (int i = 0; i < q; i++) {
-    int t, l, r;
+  for (long i = 0; i < q; i++) {
+    long t, l, r;
     cin >> t >> l >> r;
     l -= 1;
     if (t == 1) {
