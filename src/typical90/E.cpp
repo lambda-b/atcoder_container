4e68d7a1b38ca36ff169ac68dd85e99e654e10d4
lambda <shumpeta.kus@gmail.com>
2024-10-14 16:33:48 +0900

    E solve

--- src/typical90/E.cpp
+++ src/typical90/E.cpp
@@ -7,13 +7,16 @@ using namespace atcoder;
 using namespace std;
 
 using mint = modint1000000007;
+using ll = long long;
 
 struct b_mods {
-  int b, n;
+  int b;
+
+  ll n;
 
   vector<mint> mods;
 
-  b_mods(int b, int n, vector<mint> &mods) : b(b), n(n), mods(mods) {}
+  b_mods(int b, ll n, vector<mint> &mods) : b(b), n(n), mods(mods) {}
 
   mint &operator[](int i) { return mods[i]; }
 
@@ -21,7 +24,7 @@ struct b_mods {
     vector<mint> mods0(this->b);
     fill(mods0.begin(), mods0.end(), 0);
 
-    int n0 = this->n + other.n;
+    ll n0 = this->n + other.n;
     modint::set_mod(b);
     modint tb = modint(10).pow(other.n);
     for (int i = 0; i < b; i++) {
@@ -33,25 +36,20 @@ struct b_mods {
     return b_mods(this->b, n0, mods0);
   }
 
-  b_mods power(const int n) const {
-    if (n == 0) return id(this->b);
+  b_mods pow(const ll n) const {
     if (n == 1) return *this;
-    b_mods a = power(n / 2);
+    b_mods a = pow(n / 2);
     if (n % 2 == 0) return a * a;
     return a * a * (*this);
-  }
-
-  static b_mods id(int b) {
-    vector<mint> mods0(b);
-    fill(mods0.begin(), mods0.end(), 0);
-    mods0[0] = 1;
-    return b_mods(b, 0, mods0);
   }
 };
 
 int main() {
-  int n, b, k;
-  cin >> n >> b >> k;
+  ll n;
+  cin >> n;
+
+  int b, k;
+  cin >> b >> k;
 
   vector<mint> mods(b);
   fill(mods.begin(), mods.end(), 0);
@@ -62,7 +60,7 @@ int main() {
   }
   b_mods base(b, 1, mods);
 
-  b_mods ans = base.power(n);
+  b_mods ans = base.pow(n);
   cout << ans[0].val() << endl;
 
   return 0;
