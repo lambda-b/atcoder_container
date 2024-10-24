776d7735f58f4902fcd6650fb9667b6db0df9021
lambda <shumpeta.kus@gmail.com>
2024-10-25 07:36:13 +0900

    A modify

--- src/practice2/A.cpp
+++ src/practice2/A.cpp
@@ -1,5 +1,6 @@
 #include <atcoder/all>
 #include <iostream>
+#include <set>
 
 using namespace std;
 using namespace atcoder;
@@ -8,18 +9,36 @@ int main() {
   int n, q;
   cin >> n >> q;
 
-  // Union-Find
-  dsu uf(n);
+  vector<set<int> *> s(n);
+  for (int i = 0; i < n; i++) {
+    set<int> *a = new set<int>();
+    a->insert(i);
+    s[i] = a;
+  }
 
   for (int i = 0; i < q; i++) {
-    int type, u, v;
-    cin >> type >> u >> v;
+    int t, u, v;
+    cin >> t >> u >> v;
 
-    if (type == 1) {
-      cout << (uf.same(u, v) ? 1 : 0) << endl;
+    if (s[u]->size() < s[v]->size()) {
+      swap(u, v);
+    }
+
+    if (t == 1) {
+      cout << s[v]->count(u) << endl;
       continue;
     }
 
-    uf.merge(u, v);
+    if (s[v]->count(u) > 0) {
+      continue;
+    }
+
+    set<int> *c = s[v];
+    for (auto e : *c) {
+      s[u]->insert(e);
+      s[e] = s[u];
+    }
   }
+
+  return 0;
 }
